1874a2425b605855a6a2a8e9aed04b2a64d268b9
bhawkeswood <bhawkeswood@gmail.com>
2013-08-20 13:36:04 -0400

    First commit

---
+++ ruby_fundamentals.rb
@@ -0,0 +1,35 @@
+#Solution to "MAXIMUM"
+	arr.sort.last
+
+#Solution to "SUBTRACTING OUT THE SUGAR"
+	+
+
+#Solution to "YOUR FAVORITE AND MINE, FIBONACCI"
+	
+
+#Solution to "HAPPY NUMBERS"
+
+
+#Solution to "PRIME FACTORS"
+
+
+#Solution to "REVERSE"
+	'nocab yknuhc'.reverse, 'chunky bacon'
+
+#Solution to "HELLO WORLD"
+	upcase
+
+#Solution to "THERE'S NO WAY THIS WORKS"
+	(str == "My mind is going Dave"), true
+
+#Solution to "THERE'S NO WAY THIS WORKS VERSION 2"
+	str, "Hello" "World"
+
+#Solution to "BRACKETS AND SEARCHES"
+	'e']
+
+#Solution to "NO LIMIT"
+	split(',', -1)
+
+#Solution to "&& VERUSUS AND"
+	"blue"
